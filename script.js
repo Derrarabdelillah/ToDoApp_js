56dbbb418c321594bf8f36d6eaae39f40baa4340
Abdelillah <derrarabdelillah55@gmail.com>
2025-04-12 11:57:44 -0700

    files added succesfully

---
+++ script.js
@@ -0,0 +1,19 @@
+// Create Function
+// --- save data input in object and push it in the array
+// --- save data input to local storage
+// --- 
+
+
+
+let container = document.querySelector(".container");
+let form = document.querySelector(".form");
+let input = document.querySelector(".input");
+let addBtn = document.querySelector(".add");
+let tasks = document.querySelector(".tasks");
+
+// create
+// read
+// update
+// delete
+
+
